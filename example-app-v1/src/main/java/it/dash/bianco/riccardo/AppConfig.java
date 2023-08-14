892f44eafff574162fa7cf8befc2bd3bb7f2de2a
Riccardo Bianco <dash991@users.noreply.github.com>
2023-08-14 15:48:20 +0200

    Proof of concept

---
+++ example-app-v1/src/main/java/it/dash/bianco/riccardo/AppConfig.java
@@ -0,0 +1,10 @@
+package it.dash.bianco.riccardo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "it.dash.bianco.riccardo")
+public class AppConfig {
+
+}
---
+++ example-app-v1/src/main/java/it/dash/bianco/riccardo/ExampleApp.java
@@ -0,0 +1,35 @@
+package it.dash.bianco.riccardo;
+
+import java.io.IOException;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import com.petstore.dto.gen.Order;
+
+
+@SpringBootApplication
+public class ExampleApp {
+			
+	public static void main(String[] args) throws IOException {
+		SpringApplication.run(ExampleApp.class, args);
+	}
+	
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {this.main();};
+    }
+    
+    private void main()  {
+    	Order order = new Order();
+    	order.setFieldSetComplete("How does it feel to treat me like you do?");
+    	System.out.println(order.getFieldSetComplete());
+    }
+
+
+
+}
+
