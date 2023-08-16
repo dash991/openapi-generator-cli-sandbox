2003409be5ab994b2076f4e4cc02c0f7a0881e40
Riccardo Bianco <dash991@users.noreply.github.com>
2023-08-16 12:02:40 +0200

    Whitespace madness

--- example-app-v1/src/main/java/it/dash/bianco/riccardo/ExampleApp.java
+++ example-app-v1/src/main/java/it/dash/bianco/riccardo/ExampleApp.java
@@ -10,26 +10,22 @@ import org.springframework.context.annotation.Bean;
 
 import com.petstore.dto.gen.Order;
 
-
 @SpringBootApplication
 public class ExampleApp {
-			
+
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(ExampleApp.class, args);
 	}
-	
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {this.main();};
-    }
-    
-    private void main()  {
-    	Order order = new Order();
-    	order.setFieldSetComplete("How does it feel to treat me like you do?");
-    	System.out.println(order.getFieldSetComplete());
-    }
 
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> this.main();
+	}
 
+	private void main() {
+		Order order = new Order();
+		order.setFieldSetComplete("How does it feel to treat me like you do?");
+		System.out.println(order.getFieldSetComplete());
+	}
 
 }
-
